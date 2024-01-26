b10b3ebc66668fa5eeca2395df5b283aea6c345b
KawasakiHaruto712 <wakayama.kawasaki@gmail.com>
2024-01-26 13:37:56 +0900

    リンク切れでエラーにならない様にする

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,15 +23,21 @@ const config = {
   organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'docusaurus', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: 'ja',
+    locales: ['ja'],
+    localeConfigs: {
+      ja: {
+        label: '日本語',
+        direction: 'ltr'
+      }
+    }
   },
 
   presets: [
